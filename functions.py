d8e15e5dc4808b8a9583b4537fdfd0f8d72b677d
PaulinaG2405 <paoguzmantejada@gmail.com>
2025-03-08 09:01:48 -0500

    us-001/create_register  firts commit

---
+++ functions.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+#us-001/create_register
+
+user = []
+
+
+def user_register():
+    id = int(input("Ingrese su id"))
+    user.append(id)
+    name = input("Ingrese su nombre")
+    user.append(name)
+    last_name = input("Ingrese su apellido")
+    user.append(last_name)
+    email = input("Ingrese su email")
+    user.append(email)
+    password = input("Cree su contraseña de 8 caracteres entre numeros y letras")
+    user.append(password)
+    print(user)
+
+    
